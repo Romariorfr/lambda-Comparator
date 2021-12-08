e4c3fc617bdc6d3b421414e4b63664e5b74d9886
Romário Ferreira <romarioferreirarezende@hotmail.com>
2021-12-08 01:59:20 -0300

    exemplo-3

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -9,14 +10,23 @@ public class Program {
 
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
-		
-		list.add(new Product("TV",900.00));
-		list.add(new Product("Notebook",1200.00));
-		list.add(new Product("Tablet",450.00));
-		
-		list.sort(new MyComparator());
-		
-		for(Product p: list) {
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		Comparator<Product> comparator = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+
+		};
+
+		list.sort(comparator);
+
+		for (Product p : list) {
 			System.out.println(p);
 		}
 
